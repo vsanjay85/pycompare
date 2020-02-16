f673d8c77a18e3f608091f8c26f807dc2c493a57
Sanjay <mail@vsanjay.com>
2020-02-15 23:28:26 -0500

    version check

--- comparator.py
+++ comparator.py
@@ -1,10 +1,55 @@
 import csv
 import time
 import cv2
+import requests
+import github_repo
+import json
+import PySimpleGUI as sg
 import numpy as np
 
 #from PIL import Image as image
 
+def version_check():
+    with open('version.txt', 'r') as versionfile:
+        for line in versionfile:
+            version=line
+        git_api_url="https://api.github.com/repos/"+github_repo.fullname()+"/releases/latest"
+        response=requests.get(url=git_api_url)
+        release_ver=response.json()['tag_name']
+        if release_ver > version:
+            github_url=github_repo.remote.url()
+
+            layout = [  [sg.Text("You're running an older version of this program")],
+                        [sg.Text("Download the latest version from: "+github_url)],
+                        [sg.Button("OK")] ]
+
+            # Create the Window
+            window = sg.Window('Version Info', layout)
+            # Event Loop to process "events" and get the "values" of the inputs
+            while True:
+                event, values = window.read()
+                if event in (None, 'OK'):   # if user closes window or clicks cancel
+                    break
+                print('You entered ', values[0])
+
+            window.close()                
+
+
+
+def comparator():
+    processed_list = []
+    with open('images.csv', newline='') as csvfile:
+        img_reader = csv.reader(csvfile, delimiter=',')
+        for row in img_reader:
+            print(row[0])
+            image_a = cv2.imread(row[0])
+            image_b = cv2.imread(row[1])
+            start = time.time()
+            score = mse(image_a, image_b)
+            elapsed = str(round(time.time() - start, 4))
+            processed_list.append({"SourceImg":row[0], 'DestImg':row[1], 'SimilarityScore':score, 'Elapsed':elapsed})
+
+        csv_writer(processed_list)
 
 def mse(image_a, image_b):
     # the 'Mean Squared Error' between the two images is the
@@ -25,16 +70,7 @@ def csv_writer(processed_list):
             img_writer.writerow(item)
 
 if __name__ == "__main__":
-    processed_list = []
-    with open('images.csv', newline='') as csvfile:
-        img_reader = csv.reader(csvfile, delimiter=',')
-        for row in img_reader:
-            print(row[0])
-            image_a = cv2.imread(row[0])
-            image_b = cv2.imread(row[1])
-            start = time.time()
-            score = mse(image_a, image_b)
-            elapsed = str(round(time.time() - start, 4))
-            processed_list.append({"SourceImg":row[0], 'DestImg':row[1], 'SimilarityScore':score, 'Elapsed':elapsed})
+    version_check()
+    comparator()
+    
 
-        csv_writer(processed_list)
